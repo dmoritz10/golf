f4e2507c252372261f2078e17a77175038f098c2
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-03-13 10:04:22 -0700

    dan

--- home.js
+++ home.js
@@ -2,19 +2,6 @@
 
 async function goHome() {
 
-  var signinStatus = await gapi.auth2.getAuthInstance().isSignedIn.get()
-  console.log('signinStatus')
-  console.log(signinStatus)
-   if (!signinStatus) {
-     gotoTab('Auth')
-     return
-   }
-  
-  var hcpObj = readOption('handicapObj')
-
-  $('#hmCurrHandicap').html(hcpObj.currHandicap.toFixed(1))
-  $('#hmNbrRounds').html(hcpObj.nbrRounds)
-
   gotoTab('Home')
 
 }
