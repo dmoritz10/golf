a645293bd86bd3d89d61263f187c947240a40f84
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2024-02-24 11:49:31 -0800

    dan

--- courses.js
+++ courses.js
@@ -832,7 +832,15 @@ async function btnSCMFetchSxsHtml(e) {
 
 function updateSCMForm(sxsRtn) {
 
-  var d = sxsRtn.split('bootstrapData(').pop().split('}}});')[0] + '}}}'
+  // var d = sxsRtn.split('bootstrapData(').pop().split('}}});')[0] + '}}}'
+
+  var c = sxsRtn.split('data-page=')[1].substring(2)
+
+  console.log('c', c)
+  var d = c.substr(-28)
+
+  console.log('d', d)
+
   var sxs = JSON.parse(d).model.data
 
   // console.log(sxs)
