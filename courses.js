f294b061fde923b14d8dfaca58f8ebdc0ea36fb7
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2024-02-24 14:02:27 -0800

    dan

--- courses.js
+++ courses.js
@@ -834,10 +834,10 @@ function updateSCMForm(sxsRtn) {
 
   // var d = sxsRtn.split('bootstrapData(').pop().split('}}});')[0] + '}}}'
 
-  var b = sxsRtn.split('data-page=')[1].substring(2)
+  var b = sxsRtn.split('data-page=')[1].substring(1)
 var c = b.replace(/&quot;/g, '"')
   console.log('c', c)
-  var d = c.substr(-28)
+  var d = c.substr(-29)
 
   console.log('d', d)
 
